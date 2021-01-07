3ba72ab31bd0583552cf51b997a312792a23b84d
Aedan Cullen <aedan@pig.vision>
2021-01-07 18:39:08 -0500

    cleanup LaunchTestBangBang

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LaunchTestBangBang.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LaunchTestBangBang.java
@@ -15,14 +15,11 @@ public class LaunchTestBangBang extends OpMode {
 
     DcMotorEx launchMotor;
     double targetVelocity;
-    long lastEncoder;
-    long lastTime;
 
     final double PPR = 28.0; //Bare NeveRest pulses per revolution
 
     boolean lastY;
     boolean lastA;
-
 
     DecimalFormat df = new DecimalFormat("#.##");
 
@@ -32,24 +29,16 @@ public class LaunchTestBangBang extends OpMode {
         flywheel.start();
     }
 
-    double currentVelocity = 0;
-
     public void loop() {
-        if (System.currentTimeMillis() - lastTime > 250) {
-            currentVelocity = ((launchMotor.getCurrentPosition() - lastEncoder) / ((double)(System.currentTimeMillis() - lastTime))) * 1000.0 * 60.0 / PPR;
-            lastEncoder = launchMotor.getCurrentPosition();
-            lastTime = System.currentTimeMillis();
-        }
 
         telemetry.addData("Target velocity:", df.format(targetVelocity));
-        telemetry.addData("Current velocity:", df.format(currentVelocity));
         telemetry.addData("Current hardware velocity:", df.format(launchMotor.getVelocity() / PPR * 60.0));
         telemetry.addData("Current encoder:", launchMotor.getCurrentPosition());
 
         flywheel.setTargetRPM(targetVelocity);
 
-        if (lastY && gamepad1.y == false) targetVelocity += 50;
-        if (lastA && gamepad1.a == false) targetVelocity -= 50;
+        if (lastY && !gamepad1.y) targetVelocity += 50;
+        if (lastA && !gamepad1.a) targetVelocity -= 50;
 
         lastY = gamepad1.y;
         lastA = gamepad1.a;
