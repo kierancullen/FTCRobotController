6861eba947241ccd75aed8e7b58d770f03cdadb1
Aedan Cullen <aedan@pig.vision>
2021-01-29 20:53:58 -0500

    Add triple-odometer tracker from ftc-autopilot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutopilotTracker.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+
+public class AutopilotTracker {
+
+    public double[] getRobotPosition() {
+        return new double[3];
+    }
+
+    public double getDeltaX() {
+        return 0;
+    }
+
+    public double getDeltaY() {
+        return 0;
+    }
+
+    public double getDeltaH() {
+        return 0;
+    }
+
+    public double getDeltaPos() {
+        return 0;
+    }
+
+    public double[] getRobotAttitude() {
+        return new double[3];
+    }
+
+    public void setRobotPosition(double[] position) {
+
+    }
+
+    public void setRobotAttitude(double[] attitude) {
+
+    }
+
+    public void update() {
+
+    }
+
+}
